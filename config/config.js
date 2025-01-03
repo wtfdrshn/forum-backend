88313118fd969e7927bfebe70f2ae391696de373
Darshan Kinge <88169062+wtfdrshn@users.noreply.github.com>
2025-01-03 13:09:26 +0530

    deployment

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 const config = {
     port: process.env.PORT || 3000,
 
-    jwtSecret: process.env.JWT_SECRET || 'secret',
+    jwtSecret: process.env.JWT_SECRET,
 
-    mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/forum',
+    mongoURI: process.env.MONGO_URI,
 
     cloudName: process.env.CLOUD_NAME,
 
@@ -15,7 +15,7 @@ const config = {
     clientUrl: process.env.CLIENT_URL || 'http://localhost:5174',
     serverUrl: process.env.SERVER_URL || 'http://localhost:3000',
 
-    resendApiKeys: "re_YHN4hb9i_9GohAKCeWAP9jr6o24KJgRqa",
+    resendApiKeys: process.env.RESEND_API
 
     // devMode: process.env.DEV_MODE || true,
 };
