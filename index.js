84162925ba8ed52628fa8c0cb80c8739ab3ef68d
Darshan <itsmedarshan8@gmail.com>
2025-11-23 00:53:26 +0530

    admin control frontend

--- index.js
+++ index.js
@@ -35,7 +35,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials: true,
+  }
+));
 
 app.use(express.json());
 
